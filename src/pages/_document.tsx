9a5436f1c246027fee70d7d80e7b4b3b9f1cb5a1
Oleh Usatyi <olegysik1@ukr.net>
2024-11-19 08:22:46 +0100

    update fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,12 +8,16 @@ function Document() {
                     href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Tenor+Sans&display=swap"
                     rel="stylesheet"
                 />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Prosto+One&display=swap" rel="stylesheet"/>
             </Head>
             <body>
-            <Main />
-            <NextScript />
+            <Main/>
+            <NextScript/>
             </body>
         </Html>
     );
 }
+
 export default Document;
